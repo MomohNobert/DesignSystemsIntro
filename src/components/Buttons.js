7f729c54451e4c088fefcc50e1155d8faa3876fc
Momoh Nobert <okhai0112@gmail.com>
2020-07-14 12:49:40 +0100

    created primary button

---
+++ src/components/Buttons.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const PrimaryButton = styled.button`
+    background-color: #ffffff;
+    position: relative;
+    width: 135px;
+    height: 27px;
+    border: 1px solid #000000;
+    font-size: 1rem;
+`
+
+export default PrimaryButton;
