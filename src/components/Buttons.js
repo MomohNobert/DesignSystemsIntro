39fe59ee7ab8d57e4e0b39e0c9bbd15babc49a78
Momoh Nobert <okhai0112@gmail.com>
2020-07-14 13:36:25 +0100

    properly utilizing  utils

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,29 +1,29 @@
 import styled from "styled-components";
-
-const primaryColor = "#ffffff";
-const secondaryColor = "#255c99";
-const tertiaryColor = "#7EA3CC";
+import { defaultTheme, typeScale } from "../utils";
 
 const Button = styled.button`
     position: relative;
     min-width: 135px;
     min-height: 27px;
     border: 1px solid #000000;
-    font-size: 1rem;
+    font-size: ${typeScale.paragraph};
     padding: 12px 24px;
     cursor: pointer;
     font-family: "Roboto Mono", monospace;
 `
 
 export const PrimaryButton = styled(Button)`
-    background-color: ${primaryColor};
-`
-
-export const SecondaryButton = styled(Button)`
-    background-color: ${secondaryColor};
+    background-color: ${defaultTheme.primaryColor};
     color: white;
 `
 
+export const SecondaryButton = styled(Button)`
+    background: none;
+    border: 2px solid ${defaultTheme.primaryColor};
+`
+
 export const TertiaryButton = styled(Button)`
-    background-color: ${tertiaryColor};
+    background: none;
+    border: none;
+    color: ${defaultTheme.primaryColor}
 `
