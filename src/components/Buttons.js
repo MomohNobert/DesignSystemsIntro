f3fd28a027bdf50932c94bf6086701632f881253
Momoh Nobert <okhai0112@gmail.com>
2020-07-14 13:14:13 +0100

    created secondary and tertiary button styles

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,12 +1,29 @@
 import styled from "styled-components";
 
-const PrimaryButton = styled.button`
-    background-color: #ffffff;
+const primaryColor = "#ffffff";
+const secondaryColor = "#255c99";
+const tertiaryColor = "#7EA3CC";
+
+const Button = styled.button`
     position: relative;
-    width: 135px;
-    height: 27px;
+    min-width: 135px;
+    min-height: 27px;
     border: 1px solid #000000;
     font-size: 1rem;
+    padding: 12px 24px;
+    cursor: pointer;
+    font-family: "Roboto Mono", monospace;
 `
 
-export default PrimaryButton;
+export const PrimaryButton = styled(Button)`
+    background-color: ${primaryColor};
+`
+
+export const SecondaryButton = styled(Button)`
+    background-color: ${secondaryColor};
+    color: white;
+`
+
+export const TertiaryButton = styled(Button)`
+    background-color: ${tertiaryColor};
+`
